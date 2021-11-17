8e7f48238176006a397bfb9c6bbe35fbb6a97543
Alex Yip <Alex.Yip@softwire.com>
2021-11-17 11:09:39 +0000

    Part 1 done

--- fizzbuzz/Program.cs
+++ fizzbuzz/Program.cs
@@ -2,11 +2,26 @@
 
 namespace fizzbuzz
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (var i = 0; i < 100; i++)
+            {
+                var output = "";
+                var curr = i + 1;
+
+                if (curr % 3 == 0)
+                    output += "Fizz";
+
+                if (curr % 5 == 0)
+                    output += "Buzz";
+
+                if (output == "")
+                    output += curr.ToString();
+
+                Console.Out.WriteLine(output);
+            }
         }
     }
 }
