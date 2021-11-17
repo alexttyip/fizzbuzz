9f06dcb9605b096e1e760901bd858bf7d117ea17
Alex Yip <alex.yip@softwire.com>
2021-11-17 11:33:12 +0000

    Used dictionary to store replacements

--- fizzbuzz/Program.cs
+++ fizzbuzz/Program.cs
@@ -5,6 +5,15 @@ namespace fizzbuzz
 {
     internal class Program
     {
+        private static Dictionary<int, string> replacements = new Dictionary<int, string>()
+        {
+            {3, "Fizz"},
+            {5, "Buzz"},
+            {7, "Bang"},
+            {11, "Bong"},
+            {13, "Fezz"}
+        };
+
         private static void Main(string[] args)
         {
             var output = new List<string>();
@@ -15,18 +24,18 @@ namespace fizzbuzz
                 var curr = i + 1;
 
                 if (curr % 3 == 0)
-                    output.Add("Fizz");
+                    output.Add(replacements[3]);
 
                 if (curr % 5 == 0)
-                    output.Add("Buzz");
+                    output.Add(replacements[5]);
 
                 if (curr % 7 == 0)
-                    output.Add("Bang");
+                    output.Add(replacements[7]);
 
                 if (curr % 11 == 0)
                 {
                     output.Clear();
-                    output.Add("Bong");
+                    output.Add(replacements[11]);
                 }
 
                 if (curr % 13 == 0)
@@ -35,11 +44,11 @@ namespace fizzbuzz
 
                     if (bIdx >= 0)
                     {
-                        output.Insert(bIdx, "Fezz");
+                        output.Insert(bIdx, replacements[13]);
                     }
                     else
                     {
-                        output.Add("Fezz");
+                        output.Add(replacements[13]);
                     }
                 }
 
