8aa5eebf20a30f9a1e0b9930fe7d7e08c89900e2
Alex Yip <alex.yip@softwire.com>
2021-11-17 17:06:13 +0000

    Made changes after feedback

--- fizzbuzz/Program.cs
+++ fizzbuzz/Program.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace fizzbuzz
+namespace FizzBuzz
 {
     internal class Program
     {
-        private static Dictionary<int, string> replacements = new()
+        private static readonly Dictionary<int, string> Replacements = new()
         {
             {3, "Fizz"},
             {5, "Buzz"},
@@ -17,39 +17,54 @@ namespace fizzbuzz
         private static void Main(string[] args)
         {
             // Dictionary to store whether the user wants the multiples of these numbers to be replaced.
-            Dictionary<string, bool> optionalRules = new() {{"3", false}, {"5", false}, {"7", false}};
+            Dictionary<int, bool> optionalRules = new() {{3, false}, {5, false}, {7, false}};
 
             foreach (var s in args)
             {
-                optionalRules[s] = true;
+                try
+                {
+                    var argInt = int.Parse(s);
+                    if (optionalRules.ContainsKey(argInt))
+                    {
+                        optionalRules[argInt] = true;
+                    }
+                    else
+                    {
+                        Console.Out.WriteLine("{0} is not 3, 5, or 7. This is ignored.", argInt);
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.Out.WriteLine("{0} is not an int", s);
+                    return;
+                }
             }
 
             // Get max number from user.
             Console.Out.Write("Please enter the max number: ");
             var max = Convert.ToInt32(Console.ReadLine());
 
-            // The output list is use to temporarily store the replacements.
-            var output = new List<string>();
             for (var i = 0; i < max; i++)
             {
-                output.Clear();
+                // The output list is use to temporarily store the replacements.
+                var output = new List<string>();
 
                 var curr = i + 1;
 
                 // Check if the user wants these numbers to be replaced, if so, replace them.
-                if (optionalRules[3.ToString()] && curr % 3 == 0)
-                    output.Add(replacements[3]);
+                if (optionalRules[3] && curr % 3 == 0)
+                    output.Add(Replacements[3]);
 
-                if (optionalRules[5.ToString()] && curr % 5 == 0)
-                    output.Add(replacements[5]);
+                if (optionalRules[5] && curr % 5 == 0)
+                    output.Add(Replacements[5]);
 
-                if (optionalRules[7.ToString()] && curr % 7 == 0)
-                    output.Add(replacements[7]);
+                if (optionalRules[7] && curr % 7 == 0)
+                    output.Add(Replacements[7]);
 
                 if (curr % 11 == 0)
                 {
                     output.Clear();
-                    output.Add(replacements[11]);
+                    output.Add(Replacements[11]);
                 }
 
                 if (curr % 13 == 0)
@@ -60,12 +75,12 @@ namespace fizzbuzz
                     if (bIdx >= 0)
                     {
                         // If the element exists, insert the replacement.
-                        output.Insert(bIdx, replacements[13]);
+                        output.Insert(bIdx, Replacements[13]);
                     }
                     else
                     {
                         // If it doesn't exist, append it.
-                        output.Add(replacements[13]);
+                        output.Add(Replacements[13]);
                     }
                 }
 
